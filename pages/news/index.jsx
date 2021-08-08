9f1eb4075ee135fc8ba0a140a162ece4d8a248a0
Alex Misnovs <aleksandrmisnov@gmail.com>
2021-08-08 20:58:52 +0100

    added links to news

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 const NewsPage = () => {
   return (
     <div>
       <h1>NewsPage</h1>
+      <ul>
+        <li>
+          <Link href="/news/something-interesting">Something Interesting</Link>
+        </li>
+        <li>
+          <Link href="/news">News Page</Link>
+        </li>
+      </ul>
     </div>
   );
 };
